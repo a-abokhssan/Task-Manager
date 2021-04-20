a11322dbce2c1307e162176630980c2048409888
Amir Abokhssan <a.abokhssan@gmail.com>
2021-04-20 18:15:16 +0300

    added state machine

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,30 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
+
+  state_machine initial: :new_task do
+    event :start_develop do
+      transition [:in_qa, :new_task, :in_code_review] => :in_development
+    end
+
+    event :start_qa do
+      transition in_development: :in_qa
+    end
+
+    event :start_review do
+      transition in_qa: :in_code_review
+    end
+
+    event :complete_review do
+      transition in_code_review: :ready_for_release
+    end
+
+    event :finish_release do
+      transition ready_for_release: :released
+    end
+
+    event :archive do
+      transition [:new_task, :released] => :archived
+    end
+  end
 end
